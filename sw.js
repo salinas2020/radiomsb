9e1ed221b781afdd4f4e7188309e330b1ad714d6
salinas2020 <119802303+salinas2020@users.noreply.github.com>
2025-06-13 11:04:36 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker instalado');
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(fetch(event.request));
-});
